c53317f283069db4e8d6d1a7474c6ee9b431de2f
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-06-24 17:10:53 +0300

    add ng bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbDropdownModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Ecommerce-ng-bootstrap';
